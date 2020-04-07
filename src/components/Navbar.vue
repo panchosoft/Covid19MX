5b832dddcb396add8d4e418561b573d45cf37172
Francisco Leyva <fleyva@mty.ipointsystems.com>
2020-04-07 14:00:36 -0400

    Main Layout and Initial Map Design

---
+++ src/components/Navbar.vue
@@ -0,0 +1,35 @@
+<template>
+  <nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button
+      class="navbar-toggler"
+      type="button"
+      data-toggle="collapse"
+      data-target="#navbarsExampleDefault"
+      aria-controls="navbarsExampleDefault"
+      aria-expanded="false"
+      aria-label="Toggle navigation"
+    >
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarsExampleDefault">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item active">
+          <router-link class="nav-link" to="/"
+            >Home <span class="sr-only">(current)</span></router-link
+          >
+        </li>
+        <li class="nav-item">
+          <router-link class="nav-link" to="/about">About</router-link>
+        </li>
+      </ul>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "Navbar",
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,28 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div class="container-fluid pl-0 pr-0">
+      <div class="row">
+        <div class="col-md-9 pl-0 pr-0">
+          <MexicoMap />
+        </div>
+        <div class="col-md-3">
+          <ListByState />
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import ListByState from "@/components/ListByState.vue";
+import MexicoMap from "@/components/charts/MexicoMap.vue";
 
 export default {
   name: "Home",
   components: {
-    HelloWorld
-  }
+    ListByState,
+    MexicoMap
+  },
 };
 </script>
